6e5afcfc8136cc34be36625f5382b1a8bdd64f1b
Polina Drozdova <drozdovapb@gmail.com>
2015-04-24 23:45:42 +0300

    revised compare_vcf.py and lexicoSV.py (review commit)

--- compare_vcf.py
+++ compare_vcf.py
@@ -3,29 +3,28 @@
 import sys
 
 
+def read_vcf(filename):
+    with open(filename, 'r') as fh:
+        return [line.split('\t') for line in fh]
+
+
 def __main__():
     file1_name, file2_name, output_name = sys.argv[1:]
 
-    out = open(output_name, 'w')
-
 #reading data
+    file1 = read_vcf(file1_name)
+    file2 = read_vcf(file2_name)
 
-    with open(file1_name, 'r') as fh1:
-        file1 = [line.split('\t') for line in fh1]
-
-    with open(file2_name, 'r') as fh2:
-        file2 = [line.split('\t') for line in fh2]
-
-    file1_for_compare = [' '.join(line[:5]) for line in file1]  # only invariable information about SNPs in one string
+    file1_for_compare = [' '.join(line[:5]) for line in file1]
+    # only invariable information about SNPs in one string
 
 #The actual comparison
-
     index = 0
-
     for line2 in file2:
         elem = str(' '.join(line2[:5]))
         if elem not in file1_for_compare[index:]:
-            out.write('\t'.join(line2))
+            with open(output_name, 'w') as out:
+                out.write('\t'.join(line2))
         else:
             index += 1
 
--- lexicoSV.py
+++ lexicoSV.py
@@ -6,10 +6,7 @@ import sys
 def __main__():
     input_name, output_name = sys.argv[1:]
 
-    out = open(output_name, 'w')
-
 #importing the bed file
-
     with open(input_name, 'r') as fh1:
         features = [line.split('\t') for line in fh1]
 
@@ -17,21 +14,23 @@ def __main__():
     strange_contigs = set()
 
     for line in features:
-        if line[0] not in contigs:
-#here we fill a dictionary of contig names (keys) and chromosomes that host the genes found (values)
-            contigs[line[0]] = line[3][1]
-            continue
-        elif line[0] in contigs:
-#here we check whether second (and further genes) found in the same contig
-# elif is used instead of else for clarity (I'm not really sure about it)
-            if contigs[line[0]] == line[3][1]:
-                continue
-            else:  # if the first letter (i.e. chromosome name) differs for different genes
-                strange_contigs.add(line[0])
+        contig_name = line[0]
+        chr_letter = line[3][1]
 
-    #out.write('These contigs seem strange')
+        if contig_name not in contigs:
+        #filling a dictionary of contig names (keys) and chromosomes that host the genes (values)
+            contigs[contig_name] = chr_letter
+            continue
+        #if contig_name in contigs:
+        #here we check whether second (and further genes) found in the same contig
+        if contigs[contig_name] == chr_letter:
+            continue
+        # if the first letter (i.e. chromosome name) differs for different genes
+        strange_contigs.add(contig_name)
+
 #writing output
-    out.write('\n'.join(strange_contigs))
+    with open(output_name, 'w') as out:
+        out.write('\n'.join(strange_contigs))
 
 
 if __name__ == "__main__":
