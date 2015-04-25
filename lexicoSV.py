d85397a8b020a04286cda476440dc7672e5b5b10
Polina Drozdova <drozdovapb@gmail.com>
2015-04-25 23:08:05 +0300

    revised lexicoSV.py (review commit)

--- lexicoSV.py
+++ lexicoSV.py
@@ -3,35 +3,45 @@
 import sys
 
 
-def __main__():
-    input_name, output_name = sys.argv[1:]
+def check_for_peculiarities(list_of_features):
+    """
+    This function takes a list of features (genes and coordinates)
+    and checks whether each contig contains only adjacent genes.
+    If it is not the case it reports the name of the contig.
+    Such contigs may result from either chromosomal rearrangement
+    or assembly / annotation errors.
+    """
 
-#importing the bed file
-    with open(input_name, 'r') as fh1:
-        features = [line.split('\t') for line in fh1]
-
-    contigs = dict()
-    strange_contigs = set()
-
-    for line in features:
+    contigs = dict()  # a distionary of contig names and chromosomes
+    strange_contigs = set()  # stores names of contigs with non-adjacent genes
+    for line in list_of_features:
         contig_name = line[0]
         chr_letter = line[3][1]
 
-        if contig_name not in contigs:
-        #filling a dictionary of contig names (keys) and chromosomes that host the genes (values)
+        if contig_name not in contigs:  # fill the dictionary
             contigs[contig_name] = chr_letter
             continue
-        #if contig_name in contigs:
-        #here we check whether second (and further genes) found in the same contig
-        if contigs[contig_name] == chr_letter:
+
+        # check other genes in the same contig
+        if contigs[contig_name] == chr_letter:  # the contig is ok
             continue
-        # if the first letter (i.e. chromosome name) differs for different genes
+
         strange_contigs.add(contig_name)
 
-#writing output
+    return strange_contigs
+
+
+def main():
+    input_name, output_name = sys.argv[1:]
+
+    with open(input_name, 'r') as fh1:
+        features = [line.split('\t') for line in fh1]
+
+    strange_contigs = check_for_peculiarities(features)
+
     with open(output_name, 'w') as out:
         out.write('\n'.join(strange_contigs))
 
 
 if __name__ == "__main__":
-    __main__()
+    main()
