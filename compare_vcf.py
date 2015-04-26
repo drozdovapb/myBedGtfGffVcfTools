f841e415d38d8f6527340254014ed3db96240b62
Polina Drozdova <drozdovapb@gmail.com>
2015-04-26 12:37:19 +0300

    speedup for compare_vcf.py (review commit)

--- compare_vcf.py
+++ compare_vcf.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import sys
+import itertools
 
 
 def read_vcf(filename):
@@ -9,24 +10,24 @@ def read_vcf(filename):
 
 
 def main():
-    reference_name, mutant_name, output_name = sys.argv[1:]
+    reference_filename, mutant_filename, output_name = sys.argv[1:]
 
     #reading data
-    reference = read_vcf(reference_name)
-    mutant = read_vcf(mutant_name)
+    reference = read_vcf(reference_filename)
+    mutant = read_vcf(mutant_filename)
 
     # unique information for each variant
     reference_variants = [' '.join(line[:5]) for line in reference]
 
     #The actual comparison
-    index = 0
+    position = 0
     with open(output_name, 'w') as out:
         for line in mutant:
             variant = ' '.join(line[:5])  # unique information for this variant
-            if variant not in reference_variants[index:]:
+            if variant not in itertools.islice(reference_variants, position, None):
                     out.write('\t'.join(line))
             else:
-                index += 1
+                position += 1
 
 
 if __name__ == "__main__":
