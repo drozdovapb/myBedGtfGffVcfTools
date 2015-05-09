34a66a7243d3566f62c4eb07374c2dd15e5d72ef
Polina Drozdova <drozdovapb@gmail.com>
2015-05-09 14:12:24 +0300

    fixed some minor bugs in conversion.py (maintenance commit)

--- conversion.py
+++ conversion.py
@@ -11,32 +11,31 @@ def read_tsv(filename):
     and returns a list of lists of strings
     """
     with open(filename, 'r') as fh:
-        list_obj = []
+        list_of_lists = []
         for line in fh:
             if not line.startswith('#'):  # skip comment lines
-                line = line.rstrip()
-                list_obj.append(line.split('\t'))
-    return list_obj
+                list_of_lists.append(line.rstrip().split('\t'))
+    return list_of_lists
 
 
-def write_tsv(list_obj, output_file):
+def write_tsv(list_of_lists, filename):
     """
-    Takes a list of lists (list_obj) and writes a file (output_file)
+    Takes a list of lists (list_obj) and writes a file
     This function doesn't return anything
     """
-    with open(output_file, 'w') as out:
-        for line in list_obj:
-            out.write('\t'.join([str(x) for x in line]) + '\n')
+    with open(filename, 'w') as out:
+        for line in list_of_lists:
+            out.write('\t'.join(line) + '\n')
             # conversion to string used to avoid problems with integer coordinates
 
 
 def id_gff2_to_gff3(attributes):
     # change and save gene_id
     attributes = re.sub(r'gene_id \"(\w*).*?"', 'ID=\g<1>', attributes)
-    #change and save gene_name
+    # change and save gene_name
     attributes = re.sub(r'gene_name \"(\w*).*?"', 'Name=\g<1>', attributes)
-    #get rid of other details
-    re.sub(r'gene_.*? \"(\w*).*?\";', '', attributes)
+    # get rid of other details
+    attributes = re.sub(r'gene_.*? \"(\w*).*?\";', '', attributes)
     return attributes
 
 
@@ -52,14 +51,14 @@ def gtf2gff3(gtf2_filename, gff3_filename, cds_only=True):
     gff3 = []
     with open(gff3_filename, 'w') as out:
         for line in gtf2:
-            #print(line[-1])  # line[8] doesn't work for some strange reason but line[-1] does
-            #print(line[2:3])  # line[2:3] works but line[2] does not
+            print(line[8])  # line[8] doesn't work for some strange reason but line[-1] does
+            print(line[2])  # line[2:3] works but line[2] does not
             if cds_only:
                 if line[2:3] == ['CDS']:
-                    id_gff2_to_gff3(line)
+                    line[-1] = id_gff2_to_gff3(line[-1])
                     gff3.append(line)
             else:
-                id_gff2_to_gff3(line[-1])
+                line[-1] = id_gff2_to_gff3(line[-1])
                 gff3.append(line)
     write_tsv(gff3, gff3_filename)
     return gff3
@@ -86,7 +85,7 @@ def gff2bed(gff3_filename, bed_filename, fromfile=True):
     for line in gff3:
         #bed used 0-based coords while gff uses 1-based coords
         chrom = line[0]
-        start, stop = int(line[3])-1, int(line[4])
+        start, stop = str(int(line[3])-1), line[4]
         bed.append([chrom, start, stop])
     write_tsv(bed, bed_filename)
     return bed
@@ -95,9 +94,9 @@ def gff2bed(gff3_filename, bed_filename, fromfile=True):
 def bed2gff3(bed_filename, gff_filename, source='bed2gff'):
     """
     The idea is taken from galaxy (usegalaxy.org) but was sufficiently renewed
-    :param bed:
-    :return:
-    Source may bee explicitly specified by the user (eg the program used to obtain evidence)
+    This function takes a bed file,
+    writes a gff3 file and also returns a gff3-like object
+    Source may be explicitly specified by the user (eg the program used to obtain evidence)
     """
     bed = read_tsv(bed_filename)
     gff = []
@@ -112,18 +111,19 @@ def bed2gff3(bed_filename, gff_filename, source='bed2gff'):
         feature_type = "gene"
         phase = '.'
         if long_bed:
-            score = line[4]
-            strand = line[5]
-            attributes = 'ID=' + line[3]
+            genename, score, strand = line[3:6]
+            attributes = 'ID=' + genename
         gff.append([chrom, source, feature_type, start, end, score, strand, phase, attributes])
         # Some code to deal with introns will be here
     write_tsv(gff, gff_filename)
     return gff
 
 
+
 #if __name__ == "__main__": main()  # to be applied
 
 
+#Test section
 #Some tests that shouldn't be here
 #Everything below this line is to be replaced with proper tests
 
@@ -138,3 +138,7 @@ def bed2gff3(bed_filename, gff_filename, source='bed2gff'):
 #b = bed2gff3(path_to_test_files + "test_liftover.bed",
 #        "/media/drozdovapb/big/Studies/bioinformaticsinstitute/bio-py-14/test_out_gff3",
 #        "liftover")
+
+#gtf2gff3('/media/drozdovapb/big/Peterhof_strains_seq/Annotation/Repeats/15V_contigs.fasta.out.gff',
+#         '/media/drozdovapb/big/Peterhof_strains_seq/Annotation/Repeats/15V_repeatmasker_gff3.gff',
+#         cds_only=False)
