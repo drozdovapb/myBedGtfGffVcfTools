8fa85b8d2f1ef1fec5457f1e5ca0162905f5a407
Polina Drozdova <drozdovapb@gmail.com>
2015-06-08 14:29:57 +0300

    compare_vcf now with a function for comparison (review commit)

--- compare_vcf.py
+++ compare_vcf.py
@@ -5,39 +5,39 @@ from itertools import islice
 from io_tools import read_tsv, write_tsv
 
 
+def compare(reference, mutant):
+    position = 0
+    new_lines = []
+    for mut_line in mutant:
+        found = False
+        for ref_line in islice(reference, position, None):
+            if mut_line[:5] == ref_line[:5]:
+                found = True
+                position += 1
+                break
+        if not found:
+            new_lines.append(mut_line)
+    return new_lines
+
+
 def main():
-    reference_filename, mutant_filename, output_filename = sys.argv[1:]
 
-#    to_stdout = False
-
-    if len(sys.argv) == 4:
-        reference_filename, mutant_filename, output_filename = sys.argv[1:]
-#    elif len(sys.argv) == 3:
-#        reference_filename, mutant_filename = sys.argv[1:]
-#        to_stdout = True
+    if len(sys.argv) >= 3:
+        reference_filename, mutant_filename = sys.argv[1:3]
+        output_filename = sys.stdout
+        if len(sys.argv) == 4:
+            output_filename = sys.argv[3]
     else:
         print('Usage:' + '\n' +
               'compare_vcf.py <reference_filename> <mutant_filename> <output_filename>')
         sys.exit(2)
 
-
-    #reading data
     reference = read_tsv(reference_filename)
     mutant = read_tsv(mutant_filename)
 
-    # unique information for each variant
-    reference_variants = [' '.join(line[:5]) for line in reference]
+    result = compare(reference, mutant)
 
-    #The actual comparison
-    position = 0
-    with open(output_filename, 'w') as out:
-        for line in mutant:
-            variant = ' '.join(line[:5])  # unique information for this variant
-            if variant not in islice(reference_variants, position, None):
-                    out.write('\t'.join(line))
-            else:
-                position += 1
-
+    write_tsv(result, output_filename)
 
 if __name__ == "__main__":
     main()
