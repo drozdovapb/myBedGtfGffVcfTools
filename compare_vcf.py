68a7b4d86adff6294d60757d90d13e62fa69f7c7
Polina Drozdova <drozdovapb@gmail.com>
2015-06-19 11:59:06 +0300

    updated compare_vcf.py according to the notes (review commit)

--- compare_vcf.py
+++ compare_vcf.py
@@ -9,28 +9,28 @@ def compare(reference, mutant):
     position = 0
     new_lines = []
     for mut_line in mutant:
-        found = False
         for ref_line in islice(reference, position, None):
             if mut_line[:5] == ref_line[:5]:
-                found = True
                 position += 1
                 break
-        if not found:
+        else:
             new_lines.append(mut_line)
     return new_lines
 
 
 def main():
 
-    if len(sys.argv) >= 3:
-        reference_filename, mutant_filename = sys.argv[1:3]
-        output_filename = sys.stdout
-        if len(sys.argv) == 4:
-            output_filename = sys.argv[3]
-    else:
-        print('Usage:' + '\n' +
+    if len(sys.argv) < 3:
+        print('Usage: \n'
               'compare_vcf.py <reference_filename> <mutant_filename> <output_filename>')
         sys.exit(2)
+
+    reference_filename, mutant_filename = sys.argv[1:3]
+    output_filename = sys.stdout
+
+    if len(sys.argv) == 4:
+            output_filename = sys.argv[3]
+
 
     reference = read_tsv(reference_filename)
     mutant = read_tsv(mutant_filename)
