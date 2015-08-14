aa4c1207f51ad358c28ecce5f91c648de600e0bc
Polina Drozdova <drozdovapb@gmail.com>
2015-08-14 14:21:17 +0300

    updated renamer by table (now more flexible)

--- rename_gff_by_table.py
+++ rename_gff_by_table.py
@@ -15,13 +15,16 @@ def main():
     with open(output_gff, 'w') as out:
         for line_to_rename in to_rename:
             found = False
-            #print('fir', line_to_rename[8])
-            for perfect_line in table:
-                #print('sec', perfect_line[4][:-7])
-                #if gene name in table
-                if perfect_line[4][:-7] in line_to_rename[8]:
+            for cluster in table:
+                #exchange the next two lines if necessary (depends on the order in the table)
+                maker_name = cluster[3][:-27] #unique part of the name
+                proper_name = cluster[4]
+                if maker_name in line_to_rename[8]:
+                    #print('ldfj')
+                    #print(maker_name)
+                    #print(line_to_rename)
                     #change the next line to something more flexible
-                    out.write('\t'.join(line_to_rename[:8]) + '\t' + perfect_line[3] + '_' + strain + '\n')
+                    out.write('\t'.join(line_to_rename[:8]) + '\t' + proper_name + '_' + strain + '\n')
                     found = True
             if not found:
                 out.write('\t'.join(line_to_rename[:8]) + '\t' + 'ID=new_gene_' + str(new_gene_count) + '\n')
