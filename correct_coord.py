6e7bd89cc7527a3cb637f3fd3fc3850ab23b9392
Polina Drozdova <drozdovapb@gmail.com>
2016-03-03 14:52:03 +0300

    now coordinate correction works with bed

--- correct_coord.py
+++ correct_coord.py
@@ -15,7 +15,7 @@ def _id_gff2_to_gff3(attributes):
     return attributes
 
 
-def correct_coord(gff, out_filename):
+def correct_coord(list_of_lists, filetype, out_filename):
     """
     This function takes a gtf2 file (gtf2_filename),
     writes a gff3 file to gff3_filename
@@ -23,13 +23,17 @@ def correct_coord(gff, out_filename):
     Optimized for feeding ProteinOrtho
     """
     cnt = 0
-    for line in gff:
-        if line[3] > line[4]:
-            line[4], line[3] = line[3], line[4]
+    if filetype == "bed":
+        start, stop = 1, 2
+    elif filetype == "gff":
+        start, stop = 3, 4
+    for line in list_of_lists:
+        if line[start] > line[stop]:
+            line[stop], line[start] = line[start], line[stop]
             cnt += 1
-    io.write_tsv(gff, out_filename)
+    io.write_tsv(list_of_lists, out_filename)
     print("corrected ", str(cnt), "lines")
-    return gff
+    return list_of_lists
 
 
 def main():
@@ -45,8 +49,9 @@ def main():
     args = parser.parse_args()
 
     input_list_of_lists = io.read_tsv(args.input_filename)
+    filetype = args.input_filename[-3:]
 
-    correct_coord(input_list_of_lists, args.out)
+    correct_coord(input_list_of_lists, filetype, args.out)
 
 
 if __name__ == "__main__":
